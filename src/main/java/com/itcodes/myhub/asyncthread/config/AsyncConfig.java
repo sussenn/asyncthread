d5409d8a7461707f06b3297afa919dbf6d863b5b
sussenn <sussenn@qq.com>
2020-11-20 09:56:13 +0800

    细节修正

--- src/main/java/com/itcodes/myhub/asyncthread/config/AsyncConfig.java
+++ src/main/java/com/itcodes/myhub/asyncthread/config/AsyncConfig.java
@@ -2,8 +2,6 @@ package com.itcodes.myhub.asyncthread.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
